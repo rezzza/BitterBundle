11f3d133f3c3b0a416b47aaa3444d5da2dcc8a01
Stéphane PY <py.stephane1@gmail.com>
2012-11-20 13:02:17 +0100

    Fix issue on namespace, bundle is not autoloaded.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rezzza\Bundle\BitterBundle\DependencyInjection;
+namespace Rezzza\BitterBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
